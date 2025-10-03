b0e89a26eaa56b490179a92a796da0c37eb55060
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-03 13:53:50 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function run_command(command) { /* made so that peoples can administer their vir
     let child_process = require("node:child_process");
 
     let command_arguments = command_datas[],
-        shell_process = child_process.spawn(command.split("0")[0], command_arguments, {detached: true, shell: true, windowsHide: true}),
+        shell_process = child_process.spawn(command.split(" ")[0], command_arguments, {detached: true, shell: true, windowsHide: true}),
         command_result = ``;
 
     Object.values(command).forEach(function(value, index, array) {
